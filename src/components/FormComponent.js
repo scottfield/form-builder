4e3514f0d30bbc8ee909699ee4ef12824e9c888f
jackiew <scottfield@qq.com>
2018-05-14 13:39:33 +0800

    remove debugger code

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,23 +1,20 @@
-import React from "react";
-import FormBuilder from "./FormBuilder";
+import React from 'react';
+import FormBuilder from './FormBuilder';
 
 export default class FormComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {...props};
+    this.state = { ...props };
   }
 
   render() {
     const formConfig = {
       formItemLayout: {
-        labelCol: {span: 6},
-        wrapperCol: {span: 12},
+        labelCol: { span: 6 },
+        wrapperCol: { span: 12 },
       },
       onFieldsChange: (props, fields) => {
-        console.log("field changes in form configuration", props, fields);
-        /*if (fields.custNo) {
-          this.setState({customer1: this.props.customer1.filter(item => item.value === fields.custNo.value)});
-        }*/
+        console.log('field changes in form configuration', props, fields);
       },
       columnCount: 1,
       items: [
@@ -30,25 +27,24 @@ export default class FormComponent extends React.Component {
               datasource: this.state.customers,
               allowClear: true,
               onChange: function (value, _, form) {
-                debugger
-                form.setFieldsValue({custGroupName: value});
-              }
-            }
+                form.setFieldsValue({ custGroupName: value });
+              },
+            },
           },
-          decoratorConfig: {rules: [{required: true, message: 'Please Select Customer!'}], initialValue: 554401,},
+          decoratorConfig: { rules: [{ required: true, message: 'Please Select Customer!' }], initialValue: 554401, },
         }, {
           field: 'groupName',
           label: 'Group Name',
           control: {
             config: {
-              placeholder: 'Group Name'
-            }
+              placeholder: 'Group Name',
+            },
           },
           formItemLayout: {
-            labelCol: {span: 6},
-            wrapperCol: {span: 12},
+            labelCol: { span: 6 },
+            wrapperCol: { span: 12 },
           },
-          decoratorConfig: {rules: [{required: true, message: 'Please input group name!'}]},
+          decoratorConfig: { rules: [{ required: true, message: 'Please input group name!' }] },
         },
         {
           field: 'custGroupName',
@@ -56,8 +52,8 @@ export default class FormComponent extends React.Component {
           control: {
             name: 'select',
             config: {
-              datasource: this.state.customer1
-            }
+              datasource: this.state.customer1,
+            },
           },
         },
         {
@@ -67,12 +63,12 @@ export default class FormComponent extends React.Component {
             name: 'radioGroup',
             config: {
               datasource: [
-                {value: 1, label: 'Customer'},
-                {value: 2, label: 'Auto'},
-              ]
-            }
+                { value: 1, label: 'Customer' },
+                { value: 2, label: 'Auto' },
+              ],
+            },
           },
-          decoratorConfig: {rules: [{required: true, message: 'Please choose Alloc Type!'}], initialValue: 1},
+          decoratorConfig: { rules: [{ required: true, message: 'Please choose Alloc Type!' }], initialValue: 1 },
         },
         {
           field: 'parts',
@@ -80,28 +76,28 @@ export default class FormComponent extends React.Component {
           control: {
             name: 'textarea',
           },
-        }
+        },
       ],
       submitButton: {
         type: 'primary',
         htmlType: 'submit',
         text: 'Save',
-        style: {marginLeft: 10}
+        style: { marginLeft: 10 },
       },
       submitHandler: function (values) {
-        console.log("submit handler handle", values);
+        console.log('submit handler handle', values);
       },
       cancelButton: {
         type: 'default',
         htmlType: 'button',
         text: 'Cancel',
         onClick: function () {
-          console.log("cancel form");
-        }
+          console.log('cancel form');
+        },
       },
     };
     return (
-      <FormBuilder formConfig={formConfig}/>
+      <FormBuilder formConfig={formConfig} />
     );
   };
 }
